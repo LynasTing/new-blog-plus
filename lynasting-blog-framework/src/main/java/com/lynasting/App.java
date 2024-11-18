b21559379b3fb8be47852bc144def9ae3466f11d
LynasTing <LynasTing@proton.me>
2024-11-18 10:54:30 +0800

    ci: 新增了公用的framework模块，并配置了maven依赖

---
+++ lynasting-blog-framework/src/main/java/com/lynasting/App.java
@@ -0,0 +1,13 @@
+package com.lynasting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

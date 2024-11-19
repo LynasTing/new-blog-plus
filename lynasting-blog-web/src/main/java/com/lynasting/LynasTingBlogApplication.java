a4392a7703cc872fafe239f8e7aacdc5afab19ab
LynasTing <LynasTing@proton.me>
2024-11-19 16:31:07 +0800

    ci: 对代码风格进行了配置，新建并连接了数据库

---
+++ lynasting-blog-web/src/main/java/com/lynasting/LynasTingBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lynasting;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynasTingBlogApplication {
+  public static void main( String[] args )
+  {
+    System.out.println("lynas");
+    System.out.println( "Hello World!" );
+  }
+}
